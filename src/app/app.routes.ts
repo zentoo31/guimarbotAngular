1508b5ece300e9932202220d67d0375b2cc933de
zentoo31 <zentoo31@gmail.com>
2024-09-27 09:30:22 -0500

    first configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { MainComponent } from './mainPages/main/main.component';
+export const routes: Routes = [
+    {path: "", redirectTo: "home", pathMatch: "full"},
+    {path: "home", component: MainComponent},
+];
