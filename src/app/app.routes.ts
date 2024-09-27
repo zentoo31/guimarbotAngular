662714d302e0a4436506e07010b8595d7f2fa1a2
zentoo31 <zentoo31@gmail.com>
2024-09-27 10:00:23 -0500

    add some features

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { MainComponent } from './mainPages/main/main.component';
 import { LoginComponent } from './mainPages/login/login.component';
 import { NotFoundComponent } from './mainPages/not-found/not-found.component';
+import { RegisterComponent } from './mainPages/register/register.component';
 export const routes: Routes = [
     {path: "", redirectTo: "home", pathMatch: "full"},
     {path: "home", component: MainComponent},
     {path: "login", component: LoginComponent},
+    {path: "register", component: RegisterComponent},
     {path: "**", component: NotFoundComponent}
 ];
