4c45059a5134bfb0262e73be40552344b888ad5a
zentoo31 <zentoo31@gmail.com>
2024-09-27 09:42:17 -0500

    add some components and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './mainPages/main/main.component';
+import { LoginComponent } from './mainPages/login/login.component';
+import { NotFoundComponent } from './mainPages/not-found/not-found.component';
 export const routes: Routes = [
     {path: "", redirectTo: "home", pathMatch: "full"},
     {path: "home", component: MainComponent},
+    {path: "login", component: LoginComponent},
+    {path: "**", component: NotFoundComponent}
 ];
