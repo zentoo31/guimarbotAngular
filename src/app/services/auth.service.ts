f151865964a6d325c7c70ab4bfd352d51982897b
zentoo31 <zentoo31@gmail.com>
2024-09-27 10:41:51 -0500

    auth services

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  httpClient:HttpClient = inject(HttpClient);
+  baseURL:string = "http://localhost:3000/api/auth";
 
-  constructor() { }
+  constructor() { };
+
+
+  // register():Observable<boolean>{
+  //   return true;
+  // }
+
+
 }
