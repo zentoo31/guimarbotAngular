6f4d597d6945dd76ac3bde0d84d8acbdc8982791
David <xmontanoga@ucvvirtual.edu.pe>
2024-09-28 04:29:01 -0500

    Front Avanzado

--- src/app/mainPages/main/main.component.ts
+++ src/app/mainPages/main/main.component.ts
@@ -6,8 +6,23 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [RouterLink],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrl: './main.component.css',
 })
 export class MainComponent {
+  planes: string[] = ['Mensual', 'Bimestral', 'Trimestral', 'Anual'];
 
+  faq: string[] = [
+    '¿Qué es GuimarBot?',
+    'Soporte',
+    'Visión y Misión',
+    'Lenguajes utilizados',
+  ];
+  preventReload(event: Event) {
+    event.preventDefault();
+    const target = (event.target as HTMLAnchorElement).getAttribute('href') ?? "#inicio";
+    const element = document.querySelector(target);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
