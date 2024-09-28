c2500203dcdac0d92d3b3a3f380931c26e034bbb
David <xmontanoga@ucvvirtual.edu.pe>
2024-09-28 14:15:32 -0500

    Front Avanzado2

--- src/app/mainPages/main/main.component.ts
+++ src/app/mainPages/main/main.component.ts
@@ -25,4 +25,10 @@ export class MainComponent {
       element.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
   }
+  
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
