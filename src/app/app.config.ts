13c62bd55bd8f374300e94caa6c4c2ca479e9be1
zentoo31 <zentoo31@gmail.com>
2024-09-29 23:25:07 -0500

    Add some config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,24 +3,10 @@ import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideAuth, LogLevel } from 'angular-auth-oidc-client';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes, withViewTransitions()), 
               provideClientHydration(),
-              provideHttpClient(withFetch()),
-              provideAuth({
-                config: {
-                  authority: 'https://accounts.google.com',
-                  redirectUrl: typeof window !== 'undefined' ? window.location.origin : 'http://localhost:4200',
-                  postLogoutRedirectUri: typeof window !== 'undefined' ? window.location.origin : 'http://localhost:4200',
-                  clientId: '87730492966-s2pnul5lcu28ivho3qocppanpu01ek9l.apps.googleusercontent.com',
-                  scope: 'openid profile email offline_access',
-                  responseType: 'code',
-                  silentRenew: true,
-                  useRefreshToken: true,
-                  logLevel: LogLevel.Debug,
-                }
-              }),
+              provideHttpClient(withFetch())
             ]
 };
