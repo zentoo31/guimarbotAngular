35a96c46f62970c379b54c901bdac1ae711c2a2e
zentoo31 <zentoo31@gmail.com>
2024-09-30 22:17:19 -0500

    Add profile and spinner

--- src/app/dashboardPages/profile/profile.component.ts
+++ src/app/dashboardPages/profile/profile.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { AuthService } from '../../services/auth.service';
+import { SpinnerComponent } from '../../ui-components/spinner/spinner.component';
 import { Router } from '@angular/router';
 import { User } from '../../models/user';
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [SpinnerComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
@@ -15,7 +16,7 @@ export class ProfileComponent{
   router: Router = inject(Router);
   userService: UserService = inject(UserService);
   authService: AuthService = inject(AuthService);
-
+  isLoading:boolean  = true;
   ngOnInit(){
     this.loadUser();
   }
@@ -24,6 +25,7 @@ export class ProfileComponent{
     this.userService.getUser().then(
       response => {
         this.user = response;
+        this.isLoading = false;
       }
     ).catch(
       error => console.error(error)
