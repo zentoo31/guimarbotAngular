92f04249aa3f9e9e78f6c1fa73a019a201f04a7d
zentoo31 <zentoo31@gmail.com>
2024-10-03 11:58:09 -0500

    fix some bugs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,10 @@ import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes, withViewTransitions()), 
               provideClientHydration(),
-              provideHttpClient(withFetch(), withInterceptors([authInterceptor]))
+              provideHttpClient(withFetch())
             ]
 };
