0b6696eb410b12a2cf1427a1470b4cc67621a714
zentoo31 <zentoo31@gmail.com>
2024-10-06 20:30:36 -0500

    Add sub feature

---
+++ src/app/models/sub.ts
@@ -0,0 +1,11 @@
+export interface Sub {
+    owner_id: string,
+    subscription_id: string,
+    type: string,
+    start_date: string,
+    end_date: string,
+    status: string,
+    price: string,
+    max_beneficiaries: string,
+    beneficiary_user_id: string[]
+}
