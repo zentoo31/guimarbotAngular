7970d2b9592d7b4bddbff4eddf336d15033c0ef9
zentoo31 <zentoo31@gmail.com>
2024-10-09 22:24:17 -0500

    add cloudinary

--- src/app/dashboardPages/profile/profile.component.ts
+++ src/app/dashboardPages/profile/profile.component.ts
@@ -32,6 +32,60 @@ export class ProfileComponent{
     // this.loadSubs();
   }
 
+  openUploadWidget() {
+    const widget = (window as any).cloudinary.createUploadWidget(
+      {
+        cloudName: 'dmbry7maq',
+        uploadPreset: 'guimarbot',
+        sources: ['local', 'url', 'camera'],
+        multiple: false,
+        folder:'profiles',
+        cropping:true,
+        clientAllowedFormats: ['png', 'jpeg', 'jpg'],
+        language: 'es',
+        text:{
+          es: {
+            "or": "o",
+            local: {
+              "browse": "Elegir archivo",
+              "dd_title_single": "Arrastra tu archivo de imagen aquí",
+            },
+            menu: {
+                "files": "Mis archivos",
+                "web": "Dirección web",
+                "camera": "Cámara",
+            },
+            camera: {
+              "capture": "Capturar",
+                "cancel": "Cancelar",
+                "take_pic": "Tomar foto",
+                "explanation": "Permite el acceso a tu cámara para tomar una foto y subirla a la nube.",
+                "camera_error": "Error al acceder a la cámara.",
+                "retry": "Reintentar",
+            },
+            url: {
+                "inner_title": "Pega la URL de la imagen",
+                "input_placeholder": "http://sitio.remoto.ejemplo/imagenes/imagen-remota.jpg"
+            },
+            crop: {
+                "title": "Recortar imagen",
+                "crop_btn": "Recortar",
+                "skip_btn": "Saltar",
+                "reset_btn": "Reiniciar"
+            }
+          }
+        }
+      },
+      (error: any, result: any) => {
+        if (!error && result && result.event === 'success') {
+          console.log('Imagen subida con éxito: ', result.info);
+        }
+      }
+    );
+    widget.open();
+  }
+
+
   loadUser() {
     this.userService.getUser().then(
       response => {
