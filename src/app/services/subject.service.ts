fa438ae46917ec8bf0b318c8d705ce5aaecf300d
zentoo31 <zentoo31@gmail.com>
2024-11-22 00:38:38 -0500

    new changes

---
+++ src/app/services/subject.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Subject } from '../models/subject';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SubjectService {
+
+  baseURL: string = "http://localhost:3000/api/user-subject";
+  httpClient:HttpClient = inject(HttpClient);
+
+  constructor() { }
+
+  async getSubjects(): Promise<Subject[]>{
+    const subjects = await firstValueFrom(this.httpClient.get<Subject[]>(this.baseURL + '/get-all', {withCredentials: true}));
+    return subjects ?? [];
+  }
+
+}
